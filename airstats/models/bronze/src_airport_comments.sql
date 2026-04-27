d34529f693fa06c078eae9139c565118b56ef69b
celinekuttler <194842828+celinekuttler@users.noreply.github.com>
2026-04-27 13:45:39 +0000

    add bronze layer

---
+++ airstats/models/bronze/src_airport_comments.sql
@@ -0,0 +1,14 @@
+--- comments 
+--- id, airport_ref, airport_ident, date
+
+WITH raw_comments AS (
+    SELECT * FROM {{ source('airstats','comments')}}
+)
+SELECT 
+id AS comment_id,
+airport_ident,
+date AS comment_timestamp,
+member_nickname,
+subject AS comment_subject,
+body AS comment_body
+FROM raw_comments
---
+++ airstats/models/bronze/src_airports.sql
@@ -0,0 +1,16 @@
+
+WITH raw_airports AS (
+    SELECT * FROM {{ source('airstats','airports')}}
+)
+SELECT 
+id,
+ident AS airport_ident,
+type AS airport_type,
+name AS airport_name,
+latitude_deg AS airport_lat,
+longitude_deg AS airport_long,
+continent,
+iso_country,
+iso_region
+FROM raw_airports
+
